03a8c792e037979ccaf2e95318f627dce1c64222
Ishan Pandey <techisawesome36@gmail.com>
2025-07-22 14:45:47 +0530

    feat: use env variable for backend and add dev run script

--- chat-ffrontend/src/socket.js
+++ chat-ffrontend/src/socket.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client"
 
-const socket = io("http://localhost:3000");
+const socket = io(process.env.REACT_APP_API_URL);
 socket.on("connect", () => {
   console.log("✅ Connected to server:", socket.id);
 });
